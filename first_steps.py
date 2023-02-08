90d03dadceab943b429ee61f8afcd7cd01a30084
Grzegorz Torunski <g.torunski@gmail.com>
2023-02-08 11:40:18 +0100

    Add Firefox and Edge service

--- first_steps.py
+++ first_steps.py
@@ -2,11 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from time import sleep
 
+# Chrome
 chromeDriverPath = r'D:\resources\chromedriver.exe'
 chromeService = Service(chromeDriverPath)
+driver = webdriver.Chrome(service=chromeService)
+
+# Firefox
+firefoxDriverPath = r'D:\resources\geckodriver.exe'
+firefoxService = Service(firefoxDriverPath)
+#driver = webdriver.Firefox(service=firefoxService)
+
+# Edge
+edgeDriverPath = r'D:\resources\msedgedriver.exe'
+edgeService = Service(edgeDriverPath)
+#driver = webdriver.Edge(service=edgeService)
+
 url = 'https://www.google.com/'
 
-driver = webdriver.Chrome(service=chromeService)
+
 driver.maximize_window()
 driver.get(url)
 driver.get(url + "doodles")
